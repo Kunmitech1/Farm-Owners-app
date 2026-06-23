acc1436688dc91daf07105a1e4e670a3277cc521
Kunmitech1 <olumuyiwa761@gmail.com>
2026-06-23 01:04:01 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  databaseURL: "https://YOUR_DB.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "123456789",
+  appId: "YOUR_APP_ID"
+};
